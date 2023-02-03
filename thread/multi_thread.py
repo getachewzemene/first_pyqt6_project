c45b02fd5cb4ceb44b16e0a40e0706b15f4eda43
getachewzemene <getchze1221@gmail.com>
2023-02-03 16:32:21 +0300

    thread using thread IO class and status of threads

--- thread/multi_thread.py
+++ thread/multi_thread.py
@@ -1,43 +1,69 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QLabel, QWidget
-from PyQt6.QtCore import QThread, pyqtSignal, QTimer, QRunnable, QThreadPool, pyqtSlot
+from PyQt6.QtCore import QTimer, pyqtSignal, QObject, QRunnable, QThreadPool, pyqtSlot
 import time
+import traceback
+import sys
+
+# Thread using QThread IO class
+
+
+class WorkerSignals(QObject):
+
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+    progress = pyqtSignal(int)
 
 
 class Worker(QRunnable):
-    def __init__(self, *args, **kwargs):
-        super().__init__()
+
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+
+        # Store constructor arguments (re-used for processing)
+        self.fn = fn
         self.args = args
         self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+        # Add the callback to our kwargs
+        self.kwargs['progress_callback'] = self.signals.progress
 
     @pyqtSlot()
     def run(self):
-        print("Thread Start")
-        time.sleep(5)
-        print("Thread Done")
+        '''
+        Initialise the runner function with passed args, kwargs.
+        '''
+
+        # Retrieve args/kwargs here; and fire processing using them
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            # Return the result of the processing
+            self.signals.result.emit(result)
+        finally:
+            self.signals.finished.emit()  # Done
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Multi Thread")
-        self.threadpool = QThreadPool()
-        print("Multithreading with maximum %d threads" %
-              self.threadpool.maxThreadCount())
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
         self.counter = 0
 
         layout = QVBoxLayout()
 
-        self.l = QLabel("Mutli Threading is cool!")
+        self.l = QLabel("Start")
         b = QPushButton("DANGER!")
         b.pressed.connect(self.oh_no)
 
-        c = QPushButton("?")
-        c.pressed.connect(self.change_message)
-
         layout.addWidget(self.l)
         layout.addWidget(b)
-
-        layout.addWidget(c)
 
         w = QWidget()
         w.setLayout(layout)
@@ -46,15 +72,47 @@ class MainWindow(QMainWindow):
 
         self.show()
 
-    def change_message(self):
-        self.message = "OH NO"
+        self.threadpool = QThreadPool()
+        print("Multithreading with maximum %d threads" %
+              self.threadpool.maxThreadCount())
+
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.recurring_timer)
+        self.timer.start()
+
+    def progress_fn(self, n):
+        print("%d%% done" % n)
+
+    def execute_this_fn(self, progress_callback):
+        for n in range(0, 5):
+            time.sleep(1)
+            progress_callback.emit(n*100/4)
+
+        return "Done."
+
+    def print_output(self, s):
+        print(s)
+
+    def thread_complete(self):
+        print("THREAD COMPLETE!")
 
     def oh_no(self):
-        worker = Worker()
+        # Pass the function to execute
+        # Any other args, kwargs are passed to the run function
+        worker = Worker(self.execute_this_fn)
+        worker.signals.result.connect(self.print_output)
+        worker.signals.finished.connect(self.thread_complete)
+        worker.signals.progress.connect(self.progress_fn)
+
+        # Execute
         self.threadpool.start(worker)
 
+    def recurring_timer(self):
+        self.counter += 1
+        self.l.setText("Counter: %d" % self.counter)
 
-if __name__ == "__main__":
-    app = QApplication([])
-    window = MainWindow()
-    app.exec()
+
+app = QApplication([])
+window = MainWindow()
+app.exec()
